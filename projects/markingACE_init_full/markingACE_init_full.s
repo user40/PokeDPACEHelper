91b6f00093ece24b572504fbd10e2b7e3ea728b4
Gahaku <mudax2.mojikasegi@gmail.com>
2021-12-18 00:04:50 +0000

    [fix] markingACE_setupを起動するとフリーズする問題の修正

--- projects/markingACE_init_full/markingACE_init_full.s
+++ projects/markingACE_init_full/markingACE_init_full.s
@@ -1,10 +1,10 @@
 @ marking ACE entry point
-@ r7 corresponding to selected marks
+@ r3 corresponding to selected marks
 .arch armv5te
 .text
 .code	16
 .thumb_func
-ldr r7, [r4]    @ r7 = marks    
+ldrb r3, [r4]    @ r3 = marks    
 ldr r1, [sp]
 add r1, #0x9    @ 8 + 1
 blx r1
--- projects/markingACE_setup/markingACE_setup.s
+++ projects/markingACE_setup/markingACE_setup.s
@@ -15,11 +15,11 @@ start:
     bx r3
     .align 2, 0, 0
 data:
-    .word 0x99006827
+    .word 0x99007823
     .word 0x47883109
     .word 0x00000000
     .word 0x0206DAB0
-    @  206dab0:	6827      	ldr	r7, [r4, #0]
+    @  206dab0:	7823      	ldrb	r3, [r4, #0]
     @  206dab2:	9900      	ldr	r1, [sp, #0]
     @  206dab4:	3109      	adds	r1, #9
     @  206dab6:	4788      	blx	r1
